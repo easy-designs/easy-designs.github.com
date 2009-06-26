41c2d0dc5c90ea0783d75ac6e2eec4e3a0593975
aarongustafson <aaron@easy-designs.net>
2009-06-26 12:13:21 -0400

    Adding the index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Easy! Designs on GitHub</title>
+	<meta name="author" content="Aaron Gustafson">
+</head>
+<body>
+<h1>Coming Soon</h1>
+</body>
+</html>
